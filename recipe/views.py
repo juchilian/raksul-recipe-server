7731ff78054097332c19d46940d5091afb65097b
juchilian <juchilian1227@gmail.com>
2021-06-27 11:42:19 +0900

    [fix] error解消

--- recipe/views.py
+++ recipe/views.py
@@ -1,14 +1,10 @@
-from django.http.response import Http404
-from django.shortcuts import render
 from .models import Recipe
-from rest_framework import generics, serializers, status
+from rest_framework import status
 from .serializer import RecipeSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import JsonResponse
 
-
-class RecipeView(generics.ListCreateAPIView):
+class RecipeView(APIView):
     serializer_class = RecipeSerializer
 
     def get(self, request, format=None):
@@ -24,30 +20,36 @@ class RecipeView(generics.ListCreateAPIView):
             return Response({"message": "Recipe successfully created!", "recipes": serializer.data}, status=status.HTTP_201_CREATED)
         return Response({"message": "Recipe creation failed!", "required": "title, making_time, serves, ingredients, cost"}, status=status.HTTP_400_BAD_REQUEST)
 
-class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = RecipeSerializer
-
-    def get_object(self, pk):
-        try:
-            return Recipe.objects.get(pk=pk)
-        except Recipe.DoesNotExist:
-            raise Http404
-            # raise Response({ "message":"No Recipe found" }, status=status.HTTP_404_NOT_FOUND)
+class RecipeDetail(APIView):
 
     def get(self, request, pk, format=None):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe)
-        return Response({"message": "Recipe details by id", "recipe": [serializer.data]}, status=status.HTTP_200_OK)
+        try:
+            recipe = self.get_object(pk)
+            serializer = RecipeSerializer(recipe)
+            return Response({"message": "Recipe details by id", "recipe": [serializer.data]}, status=status.HTTP_200_OK)
+        except:
+            return Response({ "message":"No Recipe found" }, status=status.HTTP_404_NOT_FOUND)
+ 
 
     def patch(self, request, pk, format=None):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Recipe successfully updated!", "recipe": serializer.errors}, status=status.HTTP_200_OK)
-        return Response({"message": "Recipe creation failed!", "required": "title, making_time, serves, ingredients, cost"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            recipe = self.get_object(pk)
+            serializer = RecipeSerializer(recipe, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "Recipe successfully updated!", "recipe": [serializer.data]}, status=status.HTTP_200_OK)
+
+            return Response({"message": "Recipe update failed!", "required": "title, making_time, serves, ingredients, cost"}, status=status.HTTP_400_BAD_REQUEST)
+
+        except:
+            return Response({ "message":"No Recipe found" }, status=status.HTTP_404_NOT_FOUND)
+
+
 
     def delete(self, request, pk, format=None):
-        recipe = self.get_object(pk)
-        recipe.delete()
-        return Response({"message": "Recipe successfully removed!"}, status=status.HTTP_204_NO_CONTENT)
+        try:
+            recipe = self.get_object(pk)
+            recipe.delete()
+            return Response({"message": "Recipe successfully removed!"}, status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({ "message":"No Recipe found" }, status=status.HTTP_404_NOT_FOUND)
