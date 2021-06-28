dccbaf8b19ff78ddfdd91c765bd0333a9cd24663
juchilian <juchilian1227@gmail.com>
2021-06-28 09:54:42 +0900

    [update] url修正

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RecipeView, RecipeDetail
 
 urlpatterns = [
-    path('recipes/', RecipeView.as_view()),
+    path('recipes', RecipeView.as_view()),
     path('recipes/<int:pk>/', RecipeDetail.as_view()),
 ]
